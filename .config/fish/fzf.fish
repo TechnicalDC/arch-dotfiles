ab2544972b86cd660966d14974224581eb7a2ed5
Dilip Chauhan <chauhandilip305@gmail.com>
2026-05-19 20:17:51 +0530

    updated colorscheme

--- .config/fish/fzf.fish
+++ .config/fish/fzf.fish
@@ -1,22 +1,22 @@
 # Background tones
-set -l base00 '#1e2127', # Default Background
-set -l base01 '#282c34', # Lighter Background (status bars)
-set -l base02 '#313640', # Selection Background
-set -l base03 '#616b7f', # Comments, Invisibles
+set -l base00 '#1c1e21', # Default Background
+set -l base01 '#2f3237', # Lighter Background (status bars)
+set -l base02 '#2a2d32', # Selection Background
+set -l base03 '#66686f', # Comments, Invisibles
 # Foreground tones
-set -l base04 '#61afef', # Dark Foreground (status bars)
-set -l base05 '#cdd4e1', # Default Foreground
-set -l base06 '#cdd4e1', # Light Foreground
-set -l base07 '#cdd4e1', # Lightest Foreground
+set -l base04 '#b0b2b5', # Dark Foreground (status bars)
+set -l base05 '#f2f2f3', # Default Foreground
+set -l base06 '#f2f2f3', # Light Foreground
+set -l base07 '#f2f2f3', # Lightest Foreground
 # Accent colors
-set -l base08 '#e06c75', # Variables, XML Tags, Errors
-set -l base09 '#98c379', # Integers, Constants
-set -l base0A '#c678dd', # Classes, Search Background
-set -l base0B '#61afef', # Strings, Diff Inserted
-set -l base0C '#b9e996', # Regex, Escape Chars
-set -l base0D '#8bc4f3', # Functions, Methods
-set -l base0E '#d696e9', # Keywords, Storage
-set -l base0F '#e06c75', # Deprecated, Embedded Tags
+set -l base08 '#fd4663', # Variables, XML Tags, Errors
+set -l base09 '#9391a1', # Integers, Constants
+set -l base0A '#9192a1', # Classes, Search Background
+set -l base0B '#9ea3ad', # Strings, Diff Inserted
+set -l base0C '#bcbac4', # Regex, Escape Chars
+set -l base0D '#babdc5', # Functions, Methods
+set -l base0E '#babbc4', # Keywords, Storage
+set -l base0F '#fd4663', # Deprecated, Embedded Tags
 
 set --export FZF_DEFAULT_OPTS " --color=bg+:$base00,bg:$base00,spinner:$base0C,hl:$base0D"\
 " --color=fg:$base05,header:$base0D,info:$base0A,pointer:$base0C,label:$base08"\
