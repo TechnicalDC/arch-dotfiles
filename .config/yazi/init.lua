bfbe76b9547156c7182da5d96833be32ddc9acf7
Dilip Chauhan <chauhandilip305@gmail.com>
2026-05-23 23:00:21 +0530

    updated

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,5 +1,5 @@
 require("full-border"):setup({
-	type = ui.Border.ROUNDED
+	type = ui.Border.PLAIN
 })
 
 -- YATLINE {{{
@@ -134,32 +134,32 @@ require("eza-preview"):setup({
 
 -- Display information associated with the file/folder to mirror the output
 -- of the 'ls -l' command. i.e. permissions, hardlinks, owner, size, mtime
-function Linemode:ls_equivalent()
-
-	local permissions = self._file.cha:perm() or ""
-
-	local nlink = self._file.cha.nlink or ""
-
-	local user = ya.user_name and ya.user_name(self._file.cha.uid) or self._file.cha.uid
-	local group = ya.group_name and ya.group_name(self._file.cha.gid) or self._file.cha.gid
-	local owner = string.format("%s:%s", user, group)
-
-	local mtime = math.floor(self._file.cha.mtime or 0)
-	if mtime == 0 then
-		mtime = ""
-	elseif os.date("%Y", mtime) == os.date("%Y") then
-		mtime = os.date("%b %d %H:%M", mtime)
-	else
-		mtime = os.date("%b %d  %Y", mtime)
-	end
-
-	local size = self._file:size()
-	if size then
-		size = ya.readable_size(size) or "-"
-	else
-		local folder = cx.active:history(self._file.url)
-		size = folder and tostring(#folder.files) or ""
-	end
-
-	return string.format("%s %s %s %6s %s", permissions, nlink, owner, size, mtime)
-end
+-- function Linemode:ls_equivalent()
+--
+-- 	local permissions = self._file.cha:perm() or ""
+--
+-- 	local nlink = self._file.cha.nlink or ""
+--
+-- 	local user = ya.user_name and ya.user_name(self._file.cha.uid) or self._file.cha.uid
+-- 	local group = ya.group_name and ya.group_name(self._file.cha.gid) or self._file.cha.gid
+-- 	local owner = string.format("%s:%s", user, group)
+--
+-- 	local mtime = math.floor(self._file.cha.mtime or 0)
+-- 	if mtime == 0 then
+-- 		mtime = ""
+-- 	elseif os.date("%Y", mtime) == os.date("%Y") then
+-- 		mtime = os.date("%b %d %H:%M", mtime)
+-- 	else
+-- 		mtime = os.date("%b %d  %Y", mtime)
+-- 	end
+--
+-- 	local size = self._file:size()
+-- 	if size then
+-- 		size = ya.readable_size(size) or "-"
+-- 	else
+-- 		local folder = cx.active:history(self._file.url)
+-- 		size = folder and tostring(#folder.files) or ""
+-- 	end
+--
+-- 	return string.format("%s %s %s %6s %s", permissions, nlink, owner, size, mtime)
+-- end
