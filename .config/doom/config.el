ebd4f45b8775c70d2c827315eac808ecdbee1bcc
Dilip Chauhan <chauhandilip305@gmail.com>
2026-06-30 12:48:48 +0530

    changed theme to noctalia

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -37,7 +37,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'noctalia)
 
 ;; How tall the mode-line should be. It's only respected in GUI.
 ;; If the actual char height is larger, it respects the actual height.
