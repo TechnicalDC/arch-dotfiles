b6ab55ba6c3a2ed1c386d59cbcf2e9e222a6107a
Dilip Chauhan <chauhandilip305@gmail.com>
2026-09-14 23:16:13 +0530

    updated

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -22,6 +22,30 @@
 ;;       '(mode-line ((t (:family "Iosevka Charon Mono" ))))
 ;;       '(mode-line-active ((t (:family "Iosevka Charon Mono" ))))
 ;;       '(mode-line-inactive ((t (:family "Iosevka Charon Mono" )))))
+(defun doom-dashboard-draw-ascii-emacs-banner-fn ()
+  (let* ((banner
+          '("      __                          __                             "
+            "     /\\ \\                        /\\ \\__                          "
+            "  ___\\ \\ \\___     ___   _____    \\ \\ ,_\\  _ __    __      ___    "
+            " /'___\\ \\  _ `\\  / __`\\/\\ '__`\\   \\ \\ \\/ /\\`'__\\/'__`\\  /' _ `\\  "
+            "/\\ \\__/\\ \\ \\ \\ \\/\\ \\L\\ \\ \\ \\L\\ \\   \\ \\ \\_\\ \\ \\//\\ \\L\\.\\_/\\ \\/\\ \\ "
+            "\\ \\____\\\\ \\_\\ \\_\\ \\____/\\ \\ ,__/    \\ \\__\\\\ \\_\\\\ \\__/.\\_\\ \\_\\ \\_\\"
+            " \\/____/ \\/_/\\/_/\\/___/  \\ \\ \\/      \\/__/ \\/_/ \\/__/\\/_/\\/_/\\/_/"
+            "                          \\ \\_\\                                  "
+            "                           \\/_/                                  "
+            "                                                                 "))
+         (longest-line (apply #'max (mapcar #'length banner))))
+    (put-text-property
+     (point)
+     (dolist (line banner (point))
+       (insert (+doom-dashboard--center
+                +doom-dashboard--width
+                (concat
+                 line (make-string (max 0 (- longest-line (length line)))
+                                   32)))
+               "\n"))
+     'face 'doom-dashboard-banner)))
+;; (setq +doom-dashboard-ascii-banner-fn #'doom-dashboard-draw-ascii-emacs-banner-fn)
 
 (setq +dashboard-functions
       `(+dashboard-widget-banner
